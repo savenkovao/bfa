8594514045e3502e05f8521e24a3aaf6f2ff23a9
Artem <as@attractgroup.com>
2018-08-02 11:57:24 +0300

    fix

--- resources/js/Components/YoutubeVideoFrame.js
+++ resources/js/Components/YoutubeVideoFrame.js
@@ -5,16 +5,26 @@ const apiKey = "AIzaSyB7tcZu1wXjmpBk7Ku5Fx8A8N4jq_X_PRI";
 export class YoutubeVideoFrame {
 
     constructor() {
+      this.count = 0;
       this._initFrame();
       this._initYoutubeApi();
     }
 
     _initFrame() {
-      $.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D'+channelID, function(data) {
-        let link = data.items[0].link,
-          id = link.substr(link.indexOf("=")+1);
-        $("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
-      });
+      this.count++;
+      $.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D'+channelID).then(
+        (res)=>{
+          let link = res.items[0].link,
+            id = link.substr(link.indexOf("=")+1);
+          $("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+        },
+        (err)=>{
+          if(this.count < 10) {
+            setTimeout(()=>{
+              this._initFrame();
+            }, 1000);
+          }
+        });
     }
 
 
